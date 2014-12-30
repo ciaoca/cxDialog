59de6b186d098452f7ece08fb9f04bcef71933bf
ciaoca <ciaoca@gmail.com>
2014-12-30 11:34:23 +0800

    update 1.2
    发布到 github

--- README.md
+++ README.md
@@ -1,2 +1,160 @@
-cxDialog
-========
+#jQuery cxDialog
+
+cxDialog 是基于 jQuery 的对话框插件，支持自定义外观样式。（兼容 Zepto）
+
+**版本：**
+* jQuery v1.7+
+* jQuery cxDialog v1.2
+
+文档：http://code.ciaoca.com/jquery/cxdialog/
+
+示例：http://code.ciaoca.com/jquery/cxdialog/demo/
+
+##使用方法
+###载入 CSS 文件
+```html
+<link rel="stylesheet" href="jquery.cxdialog.css">
+```
+
+###载入 JavaScript 文件
+```html
+<script src="jquery.js"></script>
+<script src="jquery.cxdialog.js"></script>
+```
+
+###调用 cxDialog
+```javascript
+$.cxDialog('内容');
+
+$.cxDialog('内容', function(){
+    // click ok callback
+}, function(){
+    // click no callback
+});
+
+$.cxDialog({
+    title: '标题',
+    info: '内容',
+    ok: function(){
+        // code
+    },
+    no: function(){}
+});
+```
+
+###设置全局默认值
+``` javascript
+$.cxDialog.defaults.title = '提示';
+```
+
+##参数说明
+<table class="manual_table table_form">
+	<thead>
+		<tr>
+			<th width="160">名称</th>
+			<th width="160">默认值</th>
+			<th>说明</th>
+		</tr>
+	</thead>
+	<tr>
+		<td>title</td>
+		<td>''</td>
+		<td>标题</td>
+	</tr>
+	<tr>
+		<td>info</td>
+		<td>''</td>
+		<td>内容。可设置为文本内容，或 DOM 元素。</p>
+		</td>
+	</tr>
+	<tr>
+		<td>ok</td>
+		<td>null</td>
+		<td>点击确认时的回调函数</td>
+	</tr>
+	<tr>
+		<td>okText</td>
+		<td>'确 定'</td>
+		<td>确认按钮显示的文字</td>
+	</tr>
+	<tr>
+		<td>no</td>
+		<td>null</td>
+		<td>点击取消时的回调函数</td>
+	</tr>
+	<tr>
+		<td>noText</td>
+		<td>'取 消'</td>
+		<td>取消按钮显示的文字</td>
+	</tr>
+	<tr>
+		<td>buttons</td>
+		<td>[]</td>
+		<td>自定义按钮</td>
+	</tr>
+	<tr>
+		<td>closeBtn</td>
+		<td>true</td>
+		<td>是否显示关闭按钮</td>
+	</tr>
+	<tr>
+		<td>lockScroll</td>
+		<td>false</td>
+		<td>是否锁定滚动</td>
+	</tr>
+	<tr>
+		<td>baseClass</td>
+		<td>''</td>
+		<td>给对话框容器增加 class，不会覆盖默认的 class。</td>
+	</tr>
+	<tr>
+		<td>background</td>
+		<td>''</td>
+		<td><p>遮罩背景的颜色，留空表示不需要遮罩。</p>
+			<p>可设置为：HEX、RGBA 等 CSS background 属性支持的值。</p>
+		</td>
+	</tr>
+	<tr>
+		<td>width</td>
+		<td>0</td>
+		<td>提示框固定宽度</td>
+	</tr>
+	<tr>
+		<td>height</td>
+		<td>0</td>
+		<td>提示框固定高度</td>
+	</tr>
+	<tr>
+		<td>zIndex</td>
+		<td>0</td>
+		<td>提示框的层级</td>
+	</tr>
+</table>
+
+##buttons 属性参数
+<table>
+    <tr>
+        <th width="160">名称</th>
+        <th>说明</th>
+    </tr>
+    <tr>
+        <td>text</td>
+        <td>按钮显示的文字</td>
+    </tr>
+    <tr>
+        <td>callback</td>
+        <td>回调函数</td>
+    </tr>
+</table>
+
+###API 接口
+<table>
+    <tr>
+        <th width="160">名称</th>
+        <th>说明</th>
+    </tr>
+    <tr>
+        <td>$.cxDialog.close()</td>
+        <td>关闭对话框</td>
+    </tr>
+</table>
---
+++ js/jquery.cxdialog.js
@@ -0,0 +1,304 @@
+/*!
+ * jQuery cxDialog 1.2
+ * http://code.ciaoca.com/
+ * https://github.com/ciaoca/cxDialog
+ * E-mail: ciaoca@gmail.com
+ * Released under the MIT license
+ * Date: 2014-12-30
+ *
+ * 简易调用：$.cxDialog(string, [ok, no])
+ * 完整方法：$.cxDialog(opt)
+ * @param {object|string} opt 参数设置 or 内容
+ *    title {string} 标题
+ *    info {string|dom} 内容
+ *    ok {fn} 点击确认时的回调函数
+ *    okText {string} 确认按钮文字
+ *    no {fn} 点击取消时的回调函数
+ *    noText {string} 取消按钮文字
+ *    buttons {array} 自定义按钮：[{text: 'text', callback: fn}, ...]
+ *    closeBtn {boolean} 是否显示关闭按钮
+ *    lockScroll {boolean} 是否锁定滚动
+ *    baseClass {string} 给对话框容器增加 class，不会覆盖默认的 class
+ *    background {string} 遮罩背景的颜色，留空表示不需要遮罩
+ *    width {int} 提示框固定宽度
+ *    height {int} 提示框固定高度
+ *    zIndex {int} 提示框的层级
+ *
+ * @param {function} ok 点击确认时的回调函数
+ * @param {function} no 点击取消时的回调函数
+ */
+(function(factory){
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else {
+        factory(jQuery);
+    };
+}(function($){
+	var isIE6 = !!window.ActiveXObject && !window.XMLHttpRequest;
+
+	var dialog = {
+		dom: {},
+		isElement: function(o){
+			if (o && (typeof HTMLElement === 'function' || typeof HTMLElement === 'object') && o instanceof HTMLElement) {
+				return true;
+			} else {
+				return (o && o.nodeType && o.nodeType === 1) ? true : false;
+			};
+		},
+		isJquery: function(o){
+			return (o && o.length && (typeof jQuery === 'function' || typeof jQuery === 'object') && o instanceof jQuery) ? true : false;
+		},
+		isZepto: function(o){
+			return (o && o.length && (typeof Zepto === 'function' || typeof Zepto === 'object') && Zepto.zepto.isZ(o)) ? true : false;
+		}
+	};
+
+	dialog.init = function(opt, ok, no){
+		var _this = this;
+
+		_this.dom.docHtml = $('html');
+		_this.dom.docBody = $('body');
+		_this.dom.box = $('<div></div>', {'id': 'cxdialog', 'class': 'cxdialog'});
+		_this.dom.overlay = $('<div></div>', {'id': 'cxdialog_overlay', 'class': 'cxdialog_overlay'});
+		_this.dom.holder = $('<div></div>', {'id': 'cxdialog_holder', 'class': 'cxdialog_holder'});
+		_this.dom.title = $('<div></div>', {'class': 'cxdialog_title'});
+		_this.dom.info = $('<div></div>', {'class': 'cxdialog_info'});
+		_this.dom.btns = $('<div></div>', {'class': 'cxdialog_btns'});
+		_this.dom.closeBtn = $('<a></a>', {'rel': 'cxdialog', 'rev': 'close'});
+
+		_this.dom.box.on('click', 'a', function(){
+			var _rel = this.rel;
+			var _rev = this.rev;
+			var _result;
+
+			if (_rel !== 'cxdialog') {return};
+
+			if (_rev === 'close') {
+				_this.exit();
+
+			} else {
+				for (var i = 0, l = _this.btnCache.length; i < l; i++) {
+					if (_this.btnCache[i].name === _rev && typeof _this.btnCache[i].callback === 'function') {
+						_result = _this.btnCache[i].callback();
+						break;
+					};
+				};
+			};
+
+			if (_result !== false) {
+				_this.exit();
+			};
+
+			return false;
+		});
+	};
+
+	dialog.format = function(opt, ok, no){
+		var _this = this;
+		_this.exit();
+
+		if (typeof opt === 'string' && !opt.length) {return};
+
+		if (typeof opt === 'string' || _this.isElement(opt) || _this.isJquery(opt) || _this.isZepto(opt)) {
+			opt = {
+				info: opt
+			};
+		} else if (typeof opt !== 'object') {
+			return;
+		};
+
+		if (typeof ok === 'function') {opt.ok = ok};
+		if (typeof no === 'function') {opt.no = no};
+
+		opt = $.extend({}, $.cxDialog.defaults, opt);
+
+		_this.setContent(opt);
+		_this.show(opt);
+	};
+
+	// 设置内容
+	dialog.setContent = function(opt){
+		var _this = this;
+		var _timeStamp = new Date().getTime();
+
+		// 设置标题
+		if (typeof opt.title === 'string' && opt.title.length) {
+			_this.dom.title.html(opt.title).appendTo(_this.dom.box);
+		};
+
+		// 设置文本内容
+		_this.infoCache = undefined;
+
+		if (typeof opt.info === 'string' && opt.info.length) {
+			_this.dom.info.html(opt.info).appendTo(_this.dom.box);
+
+		// 设置内容为 DOM 元素或 jQuery 对象
+		} else if (_this.isElement(opt.info) || _this.isJquery(opt.info) || _this.isZepto(opt.info)) {
+			if (_this.isElement(opt.info)) {
+				_this.infoCache = $(opt.info);
+			} else {
+				_this.infoCache = opt.info;
+			};
+
+			var _cssFloat = _this.infoCache.css('float');
+			var _cssDisplay = _this.infoCache.css('display');
+			var _cssVisibility = _this.infoCache.css('visibility');
+
+			_this.dom.holder.css({
+				'float': _cssFloat,
+				'display': _cssDisplay,
+				'visibility': _cssVisibility,
+				'width': _this.infoCache.outerWidth(),
+				'height': _this.infoCache.outerHeight()
+			}).data({
+				'float': _cssFloat,
+				'display': _cssDisplay,
+				'visibility': _cssVisibility
+			}).insertAfter(_this.infoCache);
+
+			_this.infoCache.show().appendTo(_this.dom.box);
+		};
+
+		// 设置按钮
+		_this.btnCache = [];
+
+		if (typeof opt.ok === 'function') {
+			_this.btnCache.push({
+				name: 'btn_ok',
+				className: 'btn_ok',
+				text: opt.okText,
+				callback: opt.ok
+			});
+		};
+		if (typeof opt.no === 'function') {
+			_this.btnCache.push({
+				name: 'btn_no',
+				className: 'btn_no',
+				text: opt.noText,
+				callback: opt.no
+			});
+		};
+
+		for (var i = 0, l = opt.buttons.length; i < l; i++) {
+			_this.btnCache.push({
+				name: 'btn_' + _timeStamp + '_' + i,
+				className: 'btn_' + i,
+				text: opt.buttons[i].text,
+				callback: opt.buttons[i].callback
+			});
+		};
+
+		if (_this.btnCache.length) {
+			var _html = '';
+			for (var i = 0, l = _this.btnCache.length; i < l; i++) {
+				_html += '<a class="' + _this.btnCache[i].className + '" rel="cxdialog" rev="' + _this.btnCache[i].name + '">' + _this.btnCache[i].text + '</a>';
+			};
+			_this.dom.btns.html(_html).appendTo(_this.dom.box);
+		};
+
+		// 关闭按钮
+		if (opt.closeBtn) {
+			_this.dom.closeBtn.appendTo(_this.dom.box);
+		};
+	};
+
+	// 显示对话框
+	dialog.show = function(opt){
+		var _this = this;
+		_this.dom.docBody.append(_this.dom.box);
+
+		if (opt.lockScroll === true) {
+			_this.dom.docHtml.addClass('cxdialog_lock');
+		};
+
+		if (typeof opt.background && opt.background.length) {
+			_this.dom.overlay.css('background', opt.background).appendTo(_this.dom.docBody).show();
+		};
+		
+		var _cssAttr = {};
+		
+		// IE6 不支持 fixed，设置当前位置
+		if (isIE6) {
+			_cssAttr.top = document.documentElement.scrollTop + window.screen.availHeight / 4;
+		};
+
+		if (opt.width > 0) {
+			_cssAttr.width = opt.width;
+			_cssAttr.marginLeft = -(opt.width / 2);
+		} else {
+			_cssAttr.marginLeft = -(_this.dom.box.outerWidth() / 2);
+		};
+
+		if (opt.height > 0) {
+			_cssAttr.height = opt.height;
+			_cssAttr.marginTop = -(opt.height / 2);
+		} else {
+			_cssAttr.marginTop = -(_this.dom.box.outerHeight() / 2);
+		};
+
+		if (opt.zIndex > 0) {
+			_cssAttr.zIndex = opt.zIndex;
+		};
+
+		if (typeof opt.baseClass === 'string' && opt.baseClass.length) {
+			_this.dom.box.addClass(opt.baseClass);
+		};
+
+		_this.dom.box.css(_cssAttr).fadeIn(300);
+	};
+
+	// 归还加载的 DOM
+	dialog.backDom = function(){
+		var _this = this;
+
+		if (_this.isJquery(_this.infoCache) || _this.isZepto(_this.infoCache)) {
+			_this.infoCache.css({
+				'float': '',
+				'display': '',
+				'visibility': ''
+			}).insertAfter(_this.dom.holder);
+
+			_this.dom.holder.remove();
+		};
+	};
+
+	// 关闭对话框
+	dialog.exit = function(){
+		var _this = this;
+
+		_this.backDom();
+
+		_this.infoCache = undefined;
+		_this.btnCache = undefined;
+
+		_this.dom.box.attr('class', 'cxdialog').empty().hide();
+		_this.dom.overlay.hide();
+		_this.dom.docHtml.removeClass('cxdialog_lock');
+	};
+
+	dialog.init();
+
+	$.cxDialog = function(){
+		dialog.format.apply(dialog, arguments);
+	};
+
+	$.cxDialog.defaults = {
+		title: '',
+		info: '',
+		ok: null,
+		okText: '确 定',
+		no: null,
+		noText: '取 消',
+		buttons: [],
+		closeBtn: true,
+		lockScroll: false,
+		baseClass: '',
+		background: '',
+		width: 0,
+		height: 0
+	};
+
+	$.cxDialog.close = function(){
+		dialog.exit.apply(dialog);
+	};
+}));
